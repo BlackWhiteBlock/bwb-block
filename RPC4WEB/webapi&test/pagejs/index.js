ed6e09b77a8b35d46b0c97f7f42c6413372dbe04
rabbit <ningshaohui@126.com>
2018-08-05 21:43:55 +0800

    update demo

--- RPC4WEB/webapi&test/pagejs/index.js
+++ RPC4WEB/webapi&test/pagejs/index.js
@@ -2,7 +2,7 @@ document.write("<script src='./EOS/eoscommon.js'></script>");
 //sync
 function login()
 {
-    alert("login click");
+    //alert("login click");
     var walletname = config_wallet_name;
     var password = $("#pwdname").val()
     console.log(password);
@@ -11,9 +11,10 @@ function login()
     var data = UnlockWalletSync(body);
     if (data.status == 200 || data.status == 201)
     {
+        alert("登陆成功");
         location.href="pagecontents.html";
     } else {
-        alert("密码错误，请重新输入");  
+        //alert("密码错误，请重新输入");  
         location.href="pagecontents.html";        
     }
 };
